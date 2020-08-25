f604b39b8c2b094d5467950193a98e6003f953ab
batyss <batsy8219@gmail.com>
2020-08-25 23:08:49 +0300

    1.2 task completed

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,37 @@
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+function getResult(a,b,c) {
+    let d = b ** 2 - 4*a*c;
+    if (d < 0) {
+    	return [];
+    } else if (d === 0) {
+    	return [-b/(2*a)];
+    } else if  (d > 0) {
+    	return [(-b + Math.sqrt(d))/(2*a), (-b - Math.sqrt(d))/(2*a)];
+    }
 }
 
-function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+function getAverageMark(marks) {
+     if (marks.length <= 0) {
+    	return 0;
+    } else if (marks.length > 5) {
+    	console.log('Вы ввели слишком много оценок, максимальное количество - 5');
+    	marks.splice(5, 50);
+    }
+    
+    let sum = 0;
+    for (let i = 0; i < marks.length; i++) {
+      sum += marks[i];
+    }
+    
+    return sum / marks.length;
 }
 
-function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+function askDrink(name,dateOfBirthday) {
+    let today = new Date();
+    let year = today.getFullYear() - dateOfBirthday.getFullYear();
+    if (year > 18) {
+      result = `Не желаете ли олд-фэшн, ${name}?`;
+    } else {
+      result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    }
+    return result;
 }
