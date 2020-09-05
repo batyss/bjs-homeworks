88865a2f1ac83e71e17251f8ed6b10f6a5e96d6b
batyss <batsy8219@gmail.com>
2020-09-06 01:48:32 +0300

    2.2 task completed

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,33 @@
-//String.prototype.isPalindrome - для задачи №1
-
-function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+"use strict";
+// задача 1
+String.prototype.isPalindrome = function() {
+	let phrase = this.toLowerCase().replace(/ /g, '');
+	return phrase == phrase.split('').reverse().join('');
 }
 
+// задача 2
+function getAverageMark(marks) {
+    
+	if (marks.length === 0) {
+    	return 0;
+    } else {
+    	let sum = 0;
+    
+    	for (let i = 0; i < marks.length; i++) {
+      		sum += marks[i];
+    	}
+
+    	let average = sum / marks.length;
+    	let roundedAverage = Math.round(average);
+    	return roundedAverage;
+    }
+}
+
+// задача 3
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now = Date.now();
+    let Birthday = new Date(birthday);
+    let diff = now - +Birthday;
+    let age = diff / (365.25*86400000);
+    return age >= 18;
 }
